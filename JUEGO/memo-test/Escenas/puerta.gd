fd591bbd6ea628fbe6ac399fdccef498fa263808
mati <163131184+matifaya@users.noreply.github.com>
2026-04-27 15:50:13 -0300

    victoria.tscn en vez de Victoria.tscn

--- JUEGO/memo-test/Escenas/puerta.gd
+++ JUEGO/memo-test/Escenas/puerta.gd
@@ -4,4 +4,4 @@ extends Area2D
 func _on_body_entered(j):
 	if (j.name == "Jugador"):
 		if(j.tiene_llave):
-			get_tree().change_scene_to_file("res://Escenas/Victoria.tscn")
+			get_tree().change_scene_to_file("res://Escenas/victoria.tscn")
