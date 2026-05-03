72e84f95c0a28f4bfa7ef74b1fa277a37e2f2e12
santiagoblas <santiagob.beitia@gmail.com>
2026-05-02 22:00:52 -0300

    correccion
    buscar comentarios: #CORRECCION
    Cambié el tamaño de la puerta porque no podía ganarr!!

--- JUEGO/memo-test/Escenas/antorcha.gd
+++ JUEGO/memo-test/Escenas/antorcha.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var llave_objetivo : CanvasItem
 
 func _on_body_entered(j):
+	#CORRECCION: Una idea más sólida podría ser que el jugador decida qué hace cuando toca la antorcha en lugar de ser la antorcha la que decida
 	if (j.name == "Jugador"):
 		j.get_node("PointLight2D").texture_scale = 3.4
 		if llave_objetivo:
--- JUEGO/memo-test/Escenas/puerta.gd
+++ JUEGO/memo-test/Escenas/puerta.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 func _on_body_entered(j):
+	#CORRECCION: Esta clase de validaciones no están buenas a largo plazo en proyectos grandes #dato
 	if (j.name == "Jugador"):
 		if(j.tiene_llave):
 			get_tree().change_scene_to_file("res://Escenas/victoria.tscn")
